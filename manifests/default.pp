f2980d73e8266f1397d1cfb36d3bd68bfd58db80
Artback <artback@protonmail.com>
2020-09-24 12:23:15 +0200

    initial commit

---
+++ manifests/default.pp
@@ -0,0 +1,62 @@
+class update {
+  exec { 'apt-update':
+    command => '/usr/bin/apt-get update'
+  }
+}
+class curl {
+  package { 'curl':
+      ensure    => installed,
+      require => Exec['apt-update'],
+  }
+}
+class nodejs {
+  exec { 'node':
+    command => 'curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -',
+            provider => 'shell'
+  }
+  package { 'nodejs':
+    ensure    => installed,
+              require => Exec['apt-update'],
+  }
+}
+
+node /appserver*/ {
+  include update
+  include curl
+  include nodejs
+}
+
+node /dbserver*/ {
+    include update
+    package { 'mysql-server':
+      ensure    => installed,
+      require => Exec['apt-update'],
+    }
+    exec { 'set-mysql-password':
+      path => ['/bin', '/usr/bin'],
+      command => "mysqladmin -u root password root",
+      require => Service['mysql'],
+    }
+    service { "mysql":
+      ensure => "running",
+             enable => "true",
+             require => Package["mysql-server"],
+    }
+}
+
+node 'web.lan'  {
+  include update
+  package { 'nginx':
+    ensure    => installed,
+    require => Exec['apt-update'],
+  }
+  service { "nginx":
+    ensure => "running",
+           enable => "true",
+           require => Package["nginx"],
+  }
+}
+
+node default {
+  include update
+}
